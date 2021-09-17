a87fa872ba30127b259fa2208ce0eb00bb294b3f
yos10 <61726023+yos10@users.noreply.github.com>
2021-09-17 14:54:49 +0900

    WIP

--- my_script.py
+++ my_script.py
@@ -22,7 +22,7 @@ def calc_new_cases(dict1, dict2):
     return new_cases_dict
 
 def save_as_json(file_name, data):
-    with open('./data' + file_name, 'w', encoding='utf-8') as f:
+    with open(file_name, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
 def main():
